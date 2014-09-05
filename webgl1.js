b2815214f7fa127758fbe390e97457795d32ba49
unknown <karuppan@lab3-17.users.coe.utah.edu>
2014-09-05 16:32:43 -0600

    Mouse handling and rotation done. Lighting attempted with generated normals.

--- webgl1.js
+++ webgl1.js
@@ -61,6 +61,7 @@ var canvas;
 var program1;
 var aVertexPosition;
 var aVertexColor;
+var aVertexNormal;
 var vertBuf;
 var colBuf;
 var theta = 0.0;
@@ -71,10 +72,49 @@ var mvMatrix;
 var pMatrix;
 var points = [];
 var colors = [];
+var lastMouseX, lastMouseY;
+var mouseClicked = false;
+var rotMat = mat4();
+var normals = [];
+var normBuf;
 
 window.onload = function init(){
+	 
+	//UI
+	var mybutton = document.getElementById("button1");
+	mybutton.addEventListener("click", function(){
 
+	});
 	canvas = document.getElementById("mycanvas");
+	if(!canvas){ 
+		alert("No canvas tag");
+		return -1;
+	}
+	var dispText = document.getElementById("txtArea");
+	canvas.addEventListener("mousedown",function(event){
+		dispText.innerHTML = "x:" + event.clientX + " Y:" + event.clientY;
+		mouseClicked = true;
+		lastMouseX = event.clientX;
+		lastMouseY = event.clientY;
+	});
+	document.addEventListener("mousemove",function(){
+		if(mouseClicked){
+			var dx = event.clientX - lastMouseX;
+			var dy = event.clientY - lastMouseY;
+			dispText.innerHTML = dx + " " + dy;
+			//rotMat = mat4();
+			var rotMatX = rotate(dx, vec3(0,1,0));
+			var rotMatY = rotate(dy, vec3(1,0,0));
+			var compRotMat = mult(rotMatX, rotMatY);
+			rotMat = mult(compRotMat, rotMat);
+			lastMouseX = event.clientX;
+			lastMouseY = event.clientY;
+		}
+	});
+	
+	document.addEventListener("mouseup", function(){
+		mouseClicked = false;
+	});
 	gl = WebGLUtils.setupWebGL(canvas);
 	if ( !gl ) { alert( "WebGL isn't available" ); }
 
@@ -89,23 +129,20 @@ window.onload = function init(){
 	gl.enableVertexAttribArray(aVertexPosition);
 	aVertexColor = gl.getAttribLocation(program1, "aVertexColor");
 	gl.enableVertexAttribArray(aVertexColor);
+	aVertexNormal = gl.getAttribLocation(program1, "aVertexNormal");
+	gl.enableVertexAttribArray(aVertexNormal);
 	fTheta = gl.getUniformLocation(program1, "theta");
 	pMatrixUnif = gl.getUniformLocation(program1, "uPMatrix");
 	mvMatrixUnif = gl.getUniformLocation(program1, "uMVMatrix");
-var trans = translate ( 0.0, 0.0, -4.0);
-	mvMatrix = mult(trans, mvMatrix);
+// var trans = translate ( 0.0, 0.0, -4.0);
+	// mvMatrix = mult(trans, mvMatrix);
 	render();
 }
 
 function initBuffers(){
 	
+	
 	var vertices = [
-			vec3(0.0,  1.0,  -3.0),
-            vec3(0.5, -1.0,  -2.0),
-             vec3(-0.5, -1.0,  -2.0),
-			 vec3(0.0, -1.0, -4.0)
-			 ];
-	 vertices = [
         vec3(  0.0000,  0.0000, -1.0000 ),
         vec3(  0.0000,  0.9428,  0.3333 ),
         vec3( -0.8165, -0.4714,  0.3333 ),
@@ -122,6 +159,10 @@ function initBuffers(){
 	gl.bindBuffer(gl.ARRAY_BUFFER, colBuf);
 	gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
 	
+	normBuf = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, normBuf);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(normals),  gl.STATIC_DRAW);
+	
 }
 function render(){
 	gl.clearColor(1.0, 1.0, 1.0, 1.0);
@@ -131,20 +172,23 @@ function render(){
 	pMatrix = perspective(45, canvas.width / canvas.height, 0.1, 100.0);
 	//mat4.identity(mvMatrix);
 	
-	theta += 0.1;
+	theta += 1;
 	gl.uniform1f(fTheta, theta);
 	
+	//set mvMatrix to identity
+	mvMatrix = mat4();
+	
 	//translate to origin
-	var trans = translate ( 0.0, 0.0, 4.0);
+	var trans = translate ( 0.0, 0.0, -3.0);
 	mvMatrix = mult(trans, mvMatrix);
 	
 	//rotate
-	var rot = rotate(2, vec3(0.0, 1.0, 0.0));
-	mvMatrix = mult (rot, mvMatrix);
+	var rot = rotate(theta, vec3(0.0, 1.0, 0.0));
+	mvMatrix = mult (mvMatrix, rotMat);
 	
 	//translate back
-	var trans = translate ( 0.0, 0.0, -4.0);
-	mvMatrix = mult(trans, mvMatrix);
+	// var trans = translate ( 0.0, 0.0, -4.0);
+	// mvMatrix = mult(trans, mvMatrix);
 	
 	gl.bindBuffer(gl.ARRAY_BUFFER, vertBuf);
 	gl.vertexAttribPointer(aVertexPosition, 3, gl.FLOAT, false, 0,0);
@@ -152,10 +196,15 @@ function render(){
 	gl.bindBuffer(gl.ARRAY_BUFFER, colBuf);
 	gl.vertexAttribPointer(aVertexColor, 3, gl.FLOAT, false, 0,0);
 	
+	gl.bindBuffer(gl.ARRAY_BUFFER, normBuf);
+	gl.vertexAttribPointer(aVertexNormal, 3, gl.FLOAT, false, 0,0);
+	
 	gl.uniformMatrix4fv(pMatrixUnif, false, flatten(pMatrix));
 	gl.uniformMatrix4fv(mvMatrixUnif, false, flatten(mvMatrix));
+	
 	gl.enable(gl.DEPTH_TEST);
 	gl.drawArrays(gl.TRIANGLES, 0, points.length);
+	
 	window.requestAnimFrame(render);
 }
 
@@ -193,8 +242,17 @@ function triangle(a,b,c,color)
         vec3(1.0, 0.0, 0.0),
         vec3(0.0, 1.0, 0.0),
         vec3(0.0, 0.0, 1.0),
-        vec3(0.0, 0.0, 0.0)
+        vec3(0.5, 0.0, 0.5)
     ];
+		var ab = vec3(a,b);
+		var bc = vec3(b,c);
+		var ca = vec3(c,a);
+		var n1 = cross(ab,ca);
+		normals.push(n1);
+		var n2 = cross(ab,bc);
+		var n3 = cross(bc,ca);
+		normals.push(n2);
+		normals.push(n3);
 		colors.push(baseColors[color]);
 		points.push(a);
 		colors.push(baseColors[color]);
